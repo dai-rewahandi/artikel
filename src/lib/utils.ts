0e5947532b2f03cda76833f06547b25e060611f0
dai-rewahandi <niktodaire@gmail.com>
2025-01-29 11:40:44 +0700

    Add social share component with sharing functionality and update blog post to include share data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,35 @@
 export const author = {
-    name: 'Dai Rewahandi',
-    desc: 'Quick and easy solutions to fix computer problems, from hardware to software. Find tips and tricks here!'
-}
+  name: 'Dai Rewahandi',
+  desc: 'Quick and easy solutions to fix computer problems, from hardware to software. Find tips and tricks here!',
+};
+
+export const socialShare = [
+  {
+    name: 'twitter',
+  },
+  {
+    name: 'facebook',
+  },
+  {
+    name: 'linkedin',
+  },
+  {
+    name: 'whatsapp',
+  },
+  {
+    name: 'telegram',
+  },
+  {
+    name: 'reddit',
+  },
+  {
+    name: 'pinterest',
+  },
+];
 
 export function readTime(text: string) {
-    const wordPerMinute = 200;
-    const words = text.split(' ').length;
+  const wordPerMinute = 200;
+  const words = text.split(' ').length;
 
-    return Math.ceil(words / wordPerMinute);
+  return Math.ceil(words / wordPerMinute);
 }
